b5a65ed9ae64cd884879d513808216d2490dfb02
Altan Kurt <aaltankurt@gmail.com>
2023-04-24 10:20:40 +0300

    fix(config): change content path to include all HTML files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html", "./src/**/*.js"],
+  content: ["./**/*.html", "./src/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
